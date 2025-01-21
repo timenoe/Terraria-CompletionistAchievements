851e4159891ac80169589a1d7b4ad893b8b2a67f
Tim Long <timlong105@gmail.com>
2025-01-20 22:19:31 -0500

    Update submodule

--- CollectorAchievements.cs
+++ CollectorAchievements.cs
@@ -8,7 +8,7 @@ using Terraria.ModLoader;
 namespace CollectorAchievements
 {
 	// Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
-	public class CollectorAchievements : Mod
+	public class CollectorAchievementsMod : Mod
 	{
 
 	}
